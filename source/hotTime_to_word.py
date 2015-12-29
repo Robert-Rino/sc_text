b863673a425587bd5b537a06fdfdb475b7ae6b37
Robert-Rino <a901002666@gmail.com>
2015-12-29 14:47:00 +0800

    fix to_week

--- source/hotTime_to_word.py
+++ source/hotTime_to_word.py
@@ -1,7 +1,8 @@
-# coding=UTF-8
+#-*- coding=utf-8 -*-
 import csv,sys,os
 import jieba
 import jieba.analyse
+import jieba.posseg as pseg
 import datetime
 
 def return_big_index_list(timelineCount):
@@ -27,10 +28,12 @@ inPath = path+dirs[0]
 # sorted(dirs)
 for key,name in enumerate(dirs):
     if name == 'key_timeline.py':
-        del(dirs[key])  
-        
+        del(dirs[key])
+
 week_order_timeList={}
-week_order_timeList[3] = {}#future work all week data currently only week3
+#future work all week data currently only week3
+week_order_timeList[3] = {}
+# 將key_timeline存入week_order_timeList
 for order,file_name in enumerate(dirs):
     tmp_path = path+file_name
     week_order_timeList[3][order] = {}
@@ -49,8 +52,8 @@ for order,file_name in enumerate(dirs):
             returnTimeList.append(timeList[index_count[0]])
             returnCountList.append(countList[index_count[0]])
         week_order_timeList[3][order]['time'] = returnTimeList
-        week_order_timeList[3][order]['count'] = returnCountList 
-
+        week_order_timeList[3][order]['count'] = returnCountList
+# 分割字幕成關鍵字存入檔案
 for folder in os.listdir('../original_file'):
     print(folder + '============================')
     for filename in os.listdir('../original_file/'+ folder):
@@ -59,34 +62,35 @@ for folder in os.listdir('../original_file'):
         fw = open('../it_done/' + folder + '/' + filename,'w')
         subtitle = {}
         content_list = fr.read().splitlines()
-        
+
         file = filename.split(".txt")
-        
+
         for point in range(0,5):
             scale_start = week_order_timeList[int(folder)][int(file[0])]['time'][point]
             scale_end = scale_start + 5
-        
+
             for index, content in enumerate(content_list):
                 if index % 4 == 1:
                     # time
                     time = content.split()
                     time.pop(1)
                     start = datetime.datetime.strptime(time[0], "%H:%M:%S,%f")
-                    end = datetime.datetime.strptime(time[1], "%H:%M:%S,%f") 
+                    end = datetime.datetime.strptime(time[1], "%H:%M:%S,%f")
                     video_start = start.minute*60 + start.second
                     video_end = end.minute*60 + end.second
                     tmp_time = [video_start,video_end]
-                    
+
                     #keywords
                     words = jieba.analyse.extract_tags(content_list[index +1])
+                    # words = pseg.cut(content_list[index +1])
                     tmp_word = []
                     for word in words:
                         if word != ' ':
                             tmp_word.append(word)
-                    subtitle[video_start,video_end] = tmp_word 
-                    
+                    subtitle[video_start,video_end] = tmp_word
+
                     fw.write('(' + str(video_start) + ',' + str(video_end) + ')' + ' : ' + str(tmp_word) + '\n' )
-                    
+
                     if video_start < scale_start:
                         if video_end > scale_end:
                             #case1
@@ -101,11 +105,5 @@ for folder in os.listdir('../original_file'):
                         else:
                             #case4
                             print(subtitle[video_start,video_end])
-                
-        fr.close()
 
-            
-        
-        
-        
-        
+        fr.close()
