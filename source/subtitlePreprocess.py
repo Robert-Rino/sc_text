13d3301ac10129ffa51cbd0612e9d9950a40e384
yiweihuang <n1993212@gmail.com>
2016-03-01 20:52:25 +0800

    modify keyword schema

--- source/subtitlePreprocess.py
+++ source/subtitlePreprocess.py
@@ -1,16 +1,11 @@
 #-*- coding=utf-8 -*-
 import csv,sys,os
 import jieba
-import jieba.analyse
-import jieba.posseg as pseg
 import datetime
 
 jieba.set_dictionary('../dict/extra_dict/dict.txt.big')
-jieba.analyse.set_stop_words("../dict/pleonasm.txt")
-jieba.analyse.set_stop_words("../dict/extra_dict/stop_words.txt")
-# jieba.analyse.set_idf_path("../dict/extra_dict/idf.txt.big");
-jieba.analyse.set_idf_path("../dict/idf.txt")
-jieba.load_userdict('../dict/userdict.txt')
+jieba.set_dictionary('../dict/keyword.txt')
+keywords_dir = '../dict/userdict.txt'
 
 def return_big_index_list(timelineCount):
     tmp = []
@@ -30,6 +25,8 @@ def get_key_word(scale_start,scale_end,video_start,video_end):
 # original_file : 原始字幕黨
 # it_done : 處理好的字幕黨
 def preprocessSubtitle():
+    with open(keywords_dir) as f:
+        key_list = f.read().splitlines()
     for folder in os.listdir('../original_file'):
         print(folder + '============================')
         for filename in os.listdir('../original_file/'+ folder):
@@ -47,12 +44,13 @@ def preprocessSubtitle():
                     video_start = start.minute*60 + start.second
                     video_end = end.minute*60 + end.second
                     #keywords
-                    words = jieba.analyse.extract_tags(content_list[index +1], topK=2, withWeight=False, allowPOS=())
+                    words = jieba.cut(content_list[index +1], cut_all=False)
                     tmp_word = []
                     for word in words:
                         if word != ' ':
                             tmp_word.append(word)
-                    fw.write('(' + str(video_start) + ',' + str(video_end) + ')' + ' : ' + str(tmp_word) + '\n' )
+                    key = set(tmp_word).intersection(set(key_list))
+                    fw.write('(' + str(video_start) + ',' + str(video_end) + ')' + ' : ' + str(key) + '\n' )
             fr.close()
 
 #deal processed timeline data===================================
