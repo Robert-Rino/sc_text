3a8714a4ade5fe9c3c788ef08257271753657c18
yiweihuang <n1993212@gmail.com>
2016-04-13 16:03:22 +0800

    use phrase to cut

--- source/subtitlePreprocess.py
+++ source/subtitlePreprocess.py
@@ -2,12 +2,14 @@
 import csv
 import sys
 import os
+import re
 import jieba
 import datetime
 
 jieba.set_dictionary('../dict/extra_dict/dict.txt.big')
 jieba.set_dictionary('../dict/keyword.txt')
 keywords_dir = '../dict/userdict.txt'
+phrase_dir = '../dict/phrase.txt'
 
 # preprocess 原始字幕黨成 ((startTime,endTime):[wordList])
 # original_file : 原始字幕黨
@@ -17,6 +19,11 @@ keywords_dir = '../dict/userdict.txt'
 def preprocessSubtitle():
     with open(keywords_dir) as f:
         key_list = f.read().splitlines()
+
+    with open(phrase_dir) as f:
+        phrase_list = f.read()
+    cut_phrase = phrase_list.split('\n')
+
     for folder in os.listdir('../original_file'):
         print(folder + '============================')
         for filename in os.listdir('../original_file/' + folder):
@@ -37,8 +44,13 @@ def preprocessSubtitle():
                     words = jieba.cut(content_list[index + 1], cut_all=False)
                     tmp_word = []
                     for word in words:
-                        if word != ' ':
-                            tmp_word.append(word)
+                        if word != ' ' and word != '-' and word != '.' and word != '/' and word != '^':
+                            if re.search(word, phrase_list):
+                                new_word = filter(lambda x: word in x,
+                                                  cut_phrase)
+                                tmp_word.extend(new_word)
+                            else:
+                                tmp_word.append(word)
                     key = set(tmp_word).intersection(set(key_list))
                     fw.write('(' + str(video_start) + ',' + str(video_end)
                                  + ')' + ' : ' + str(key) + '\n')
